015df86af5643cb52a2312d8d3b6465b2d907747
Григорий Григорьев <grigoriy_grigorev56@mail.ru>
2023-11-01 22:39:43 +0500

    Add Tipe Hints.

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-'''Модуль для рандоминизации урона.'''
+"""Triple double quotes."""
 from random import randint
 
 
-def attack(char_name, char_class):
-    '''Функция атаки персонажа'''
+def attack(char_name: str, char_class: str) -> str:
+    """Triple double quotes."""
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -16,8 +16,8 @@ def attack(char_name, char_class):
     return f'{char_name} не нанёс урон противнику'
 
 
-def defence(char_name, char_class):
-    '''Функция распределения блокировки урона героя.'''
+def defence(char_name: str, char_class: str) -> str:
+    """Triple double quotes."""
     if char_class == 'warrior':
         return f'{char_name} блокировал {10 + randint(5, 10)} ед. урона'
     if char_class == 'mage':
@@ -27,8 +27,8 @@ def defence(char_name, char_class):
     return f'{char_name} не блокировал урон'
 
 
-def special(char_name, char_class):
-    '''Функция специальных умений героя в зависимости от их класса.'''
+def special(char_name: str, char_class: str) -> str:
+    """Triple double quotes."""
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -39,8 +39,8 @@ def special(char_name, char_class):
     return f'{char_name} не применил специальное умение'
 
 
-def start_training(char_name, char_class):
-    '''Функция выбора имени и класса героя.'''
+def start_training(char_name: str, char_class: str) -> str:
+    """Triple double quotes."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — великий мастер ближнего боя.')
     if char_class == 'mage':
@@ -52,7 +52,7 @@ def start_training(char_name, char_class):
           'defence — чтобы блокировать атаку противника или '
           'special — чтобы использовать свою суперсилу.')
     print('Если не хочешь тренироваться, введи команду skip.')
-    cmd = None
+    cmd: str = None
     while cmd != 'skip':
         cmd = input('Введи команду: ')
         if cmd == 'attack':
@@ -64,10 +64,10 @@ def start_training(char_name, char_class):
     return 'Тренировка окончена.'
 
 
-def choice_char_class():
-    '''Функция выбора класса героя.'''
-    approve_choice = None
-    char_class = None
+def choice_char_class() -> str:
+    """Triple double quotes."""
+    approve_choice: str = ''
+    char_class: str = ''
     while approve_choice != 'y':
         char_class = input('Введи название персонажа, '
                            'за которого хочешь играть: Воитель — warrior, '
@@ -87,16 +87,16 @@ def choice_char_class():
     return char_class
 
 
-def main():
-    '''Функция запуска игры.'''
+def main() -> str:
+    """Triple double quotes."""
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
-    char_name = input('...назови себя: ')
+    char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class = choice_char_class()
+    char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
 
 
